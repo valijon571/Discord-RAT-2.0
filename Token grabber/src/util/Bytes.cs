4f77e9df0f3ac1455c563e1308f5b5ee7e221f77
valijon571 <148867160+valijon571@users.noreply.github.com>
2024-06-07 22:58:18 +0500

    update file

---
+++ Token grabber/src/util/Bytes.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Org.BouncyCastle.Utilities
+{
+    public abstract class Bytes
+    {
+        public const int NumBits = 8;
+        public const int NumBytes = 1;
+    }
+}
